74ff3eb16f306c16ee6008cba24cd58f2df62436
OhEunTeak <giloet777@gmail.com>
2023-02-11 23:53:20 +0900

    start pf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import Header from './components/common/Header';
+import Footer from './components/common/Header';
+
+import Visual from './components/main/Visual';
+import Content from './components/main/Content';
+
+import Rooms from './components/sub/Rooms';
+import Faq from './components/sub/Faq';
+import Gallery from './components/sub/Gallery';
+import Youtube from './components/sub/Youtube';
+import Join from './components/sub/Join';
+import Location from './components/sub/Location';
+
+import './scss/main.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Switch>
+				<Route exact path='/'>
+					<Header type={'main'} />
+					<Visual />
+					<Content />
+					<Footer />
+				</Route>
+
+				<Route path='/'>
+					<Header type={'sub'} />
+				</Route>
+			</Switch>
+
+			<Route path='/rooms' component={Rooms} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/faq' component={Faq} />
+			<Route path='/location' component={Location} />
+			<Route path='/join' component={Join} />
+
+			<Footer />
+		</>
+	);
 }
 
 export default App;
