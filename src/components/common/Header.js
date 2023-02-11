84517d97c6ed51e36527bf401fdee27b37fc9098
OhEunTeak <giloet777@gmail.com>
2023-02-12 00:46:47 +0900

    헤더 추가 수정 및 반응형 + ROOMS페이지 완성

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,7 +2,14 @@ import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Header({ type }) {
-	const active = { color: 'orange' }
+	const [toggle, setToggle] = useState(false);
+
+	const clickedToggle = () => {
+		setToggle((prev) => !prev);
+	};
+
+	const active = { color: 'orange' };
+
 	return (
 		<header className={type}>
 			<div className='inner'>
@@ -45,11 +52,48 @@ function Header({ type }) {
 					</li>
 				</ul>
 
-				<button className='menuMo' /*onClick={clickedToggle}*/>
+				<button
+					className={`btnCall${toggle ? ' on' : ''}`}
+					onClick={clickedToggle}>
 					<span></span>
 					<span></span>
 					<span></span>
 				</button>
+
+				<nav className='menuMo'>
+					<ul id='gnbMo'>
+						<li>
+							<NavLink activeStyle={active} to='/rooms'>
+								ROOMS
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeStyle={active} to='/gallery'>
+								GALLERY
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeStyle={active} to='/youtube'>
+								YOUTUBE
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeStyle={active} to='/faq'>
+								FAQ
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeStyle={active} to='/location'>
+								LOCATION
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeStyle={active} to='/join'>
+								JOIN
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
 			</div>
 		</header>
 	);
