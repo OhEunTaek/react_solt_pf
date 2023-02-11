9e0dd3a6ba17f96c1040029daf42fec104806350
OhEunTeak <giloet777@gmail.com>
2023-02-12 00:32:35 +0900

    헤더

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Header({ type }) {
@@ -12,14 +12,44 @@ function Header({ type }) {
 					</NavLink>
 				</h1>
 
-				<ul id="gnb">
-					<li><NavLink activeStyle={active} to='/rooms'>ROOMS</NavLink></li>
-					<li><NavLink activeStyle={active} to='/gallery'>GALLERY</NavLink></li>
-					<li><NavLink activeStyle={active} to='/youtube'>YOUTUBE</NavLink></li>
-					<li><NavLink activeStyle={active} to='/faq'>FAQ</NavLink></li>
-					<li><NavLink activeStyle={active} to='/location'>LOCATION</NavLink></li>
-					<li><NavLink activeStyle={active} to='/join'>JOIN</NavLink></li>
+				<ul id='gnb'>
+					<li>
+						<NavLink activeStyle={active} to='/rooms'>
+							ROOMS
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={active} to='/gallery'>
+							GALLERY
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={active} to='/youtube'>
+							YOUTUBE
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={active} to='/faq'>
+							FAQ
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={active} to='/location'>
+							LOCATION
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={active} to='/join'>
+							JOIN
+						</NavLink>
+					</li>
 				</ul>
+
+				<button className='menuMo' /*onClick={clickedToggle}*/>
+					<span></span>
+					<span></span>
+					<span></span>
+				</button>
 			</div>
 		</header>
 	);
